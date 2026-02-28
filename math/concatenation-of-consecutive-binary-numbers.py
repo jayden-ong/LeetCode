5f1028f0d3b46f58005942ee1a5614db5d8325d3
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-02-28 14:07:36 -0500

    LC-concatenation-of-consecutive-binary-numbers [Runtime: N/A; Memory: N/A]

--- math/concatenation-of-consecutive-binary-numbers.py
+++ math/concatenation-of-consecutive-binary-numbers.py
@@ -1,9 +1,9 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        MOD = pow(10, 9) + 7
+        MOD = 10 ** 9 + 7
         answer = 0
         curr_multiple = 1
         for i in range(n, 0, -1):
             answer += i * curr_multiple
-            curr_multiple *= pow(2, len(format(i, 'b')))
+            curr_multiple *= 2 ** (math.floor(math.log(i, 2)) + 1)
         return answer % MOD
