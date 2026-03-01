79448d675931c388c937442f35af25022ac2b376
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-01 13:07:29 -0500

    LC-partitioning-into-minimum-number-of-deci-binary-numbers [Runtime: 86 ms; Memory: 19.8 MB]

--- string/partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ string/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,6 +1,6 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        answer = float('inf')
+        answer = float('-inf')
         for digit in n:
             answer = max(answer, int(digit))
         return answer
