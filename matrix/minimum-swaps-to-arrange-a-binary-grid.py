7fc8eb92bd2ec056e17c3822b0752b45b9cc8884
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-02 12:08:08 -0500

    LC-minimum-swaps-to-arrange-a-binary-grid [Runtime: N/A; Memory: N/A]

--- matrix/minimum-swaps-to-arrange-a-binary-grid.py
+++ matrix/minimum-swaps-to-arrange-a-binary-grid.py
@@ -3,9 +3,6 @@ class Solution:
         # Need to find a row that has n - 1 0's at the end, n - 2 0's at the end, and so on.
         # It doesn't matter what the last row is, so we can ignore index 0 of "rows_needed"
         # Want to map each row number to where each row currently is
-        desired_row_to_row = {}
-        rows_complete = 0
-        extra_row_index = None
         list_to_sort = []
         for i, row in enumerate(grid):
             num_zeros = 0
@@ -15,17 +12,7 @@ class Solution:
                 else:
                     num_zeros = 0
             
-            if len(grid) - num_zeros - 1 not in desired_row_to_row:
-                rows_complete += 1
-                desired_row_to_row[len(grid) - num_zeros - 1] = num_zeros
-                list_to_sort.append(num_zeros)
-            else:
-                extra_row_index = i
-                list_to_sort.append(0)
-
-        # print(rows_needed)
-        if rows_complete < len(grid) - 1:
-            return -1
+            list_to_sort.append(num_zeros)
         
         # There are two possibilities for the bottom row
         #   There is a row with 0 0s above the diagonal -- it becomes the last row
@@ -40,4 +27,8 @@ class Solution:
                     answer += 1
                     list_to_sort[j], list_to_sort[j + 1] = list_to_sort[j + 1], list_to_sort[j]
             # print(list_to_sort)
+        
+        for i, num in enumerate(list_to_sort):
+            if num < len(grid) - i - 1:
+                return -1
         return answer
