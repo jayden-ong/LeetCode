365527cb0f4144a5bef4104ea8dbadf224725e8a
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-07 16:48:15 -0500

    LC-minimum-number-of-flips-to-make-the-binary-string-alternating [Runtime: 263 ms; Memory: 19.8 MB]

--- dynamic-programming/minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ dynamic-programming/minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -14,7 +14,30 @@ class Solution:
         
         answer = min(ones_even + zeros_odd, ones_odd + zeros_even)
         # Throw char to the front
+        '''
         ones_odd, ones_even = ones_even, ones_odd
         zeros_odd, zeros_even = zeros_even, zeros_odd
         return min(answer, min(ones_even + zeros_odd, ones_odd + zeros_even))
+        '''
+        for i in range(len(s) - 1):
+            char = s[i]
+            if char == "0":
+                zeros_odd, zeros_even = zeros_even - 1, zeros_odd
+                if len(s) % 2 == 1:
+                    zeros_even += 1
+                else:
+                    zeros_odd += 1
+                
+                ones_odd, ones_even = ones_even, ones_odd
+            else:
+                ones_odd, ones_even = ones_even - 1, ones_odd
+                if len(s) % 2 == 1:
+                    ones_even += 1
+                else:
+                    ones_odd += 1
+                
+                zeros_odd, zeros_even = zeros_even, zeros_odd
+            
+            answer = min(answer, min(ones_even + zeros_odd, ones_odd + zeros_even))
+        return answer
             
