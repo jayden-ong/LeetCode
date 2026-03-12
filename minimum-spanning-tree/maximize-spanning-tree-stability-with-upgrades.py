325402a3d0e60d72ef453511b046d065f017be17
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-12 10:30:28 -0400

    LC-maximize-spanning-tree-stability-with-upgrades [Runtime: N/A; Memory: N/A]

--- minimum-spanning-tree/maximize-spanning-tree-stability-with-upgrades.py
+++ minimum-spanning-tree/maximize-spanning-tree-stability-with-upgrades.py
@@ -20,58 +20,41 @@ class Solution:
         # All edges with must == 1, must be included in the spanning tree
         # If an edge is required, put both nodes in the same group
         edges_by_strength = []
-        required_edges = []
-        max_strength = float('-inf')
+        dsu = UnionFind(n)
+        answer = float('inf')
+        edges_added = 0
         for node1, node2, strength, required in edges:
             if required:
-                required_edges.append((strength, node1, node2))
-                max_strength = max(max_strength, strength)
+                # If they have the same rep, we have an unnecessary edge
+                if dsu.find_rep(node1) == dsu.find_rep(node2):
+                    return -1
+                dsu.union(node1, node2)
+                answer = min(answer, strength)
+                edges_added += 1
             else:
-                edges_by_strength.append((strength, node1, node2)) 
-                max_strength = max(max_strength, 2 * strength)
+                edges_by_strength.append((strength, node1, node2))
         edges_by_strength.sort(reverse=True)
 
-        # The required edges dictate the absolute maximum answer
-        # It could still go lower
-        min_strength = answer = 0
-        def determine_possible(required_strength):
-            num_edges = 0
-            num_boosts = k
-            dsu = UnionFind(n)
-            curr_strength = float('inf')
-            # Add required edges first
-            for strength, node1, node2 in required_edges:
-                dsu.union(node1, node2)
-                curr_strength = min(curr_strength, strength)
-                num_edges += 1
-            
-            if curr_strength < required_strength:
-                return False
-            
-            if num_edges == n - 1:
-                return True
-            
-            # We can only add edges that causes a change to the reps
-            for strength, node1, node2 in edges_by_strength:
-                if dsu.find_rep(node1) != dsu.find_rep(node2):
-                    dsu.union(node1, node2)
-                    # Return false if smaller and out of boosts or boost is useless
-                    if strength < required_strength:
-                        if num_boosts == 0 or 2 * strength < required_strength:
-                            return False
-                        num_boosts -= 1
-                    
-                    num_edges += 1
-                    if num_edges == n - 1:
-                        return True
-            return False
+        if edges_added == n - 1:
+            return answer
+        # We want to figure out when we can start boosting
+        # We can add n - 1 - edges_added more edges
+        #   We want to boost the edges with the samllest strength first
+        #   Start boosting when k == n - 1 - edges_added
+        start_boosting = k == n - 1 - edges_added
 
-        answer = -1
-        while min_strength <= max_strength:
-            mid = (min_strength + max_strength) // 2
-            if determine_possible(mid):
-                answer = mid
-                min_strength = mid + 1
-            else:
-                max_strength = mid - 1
-        return answer
+        # We have already added all of the required edges
+        # The goal is to greedily add all necessary edges and giving them a boost 
+        for strength, node1, node2 in edges_by_strength:
+            if dsu.find_rep(node1) != dsu.find_rep(node2):
+                dsu.union(node1, node2)
+                edges_added += 1
+                if start_boosting:
+                    answer = min(answer, 2 * strength)
+                else:
+                    start_boosting = k == n - 1 - edges_added
+                
+                if edges_added == n - 1:
+                    return answer
+        return -1
+        
