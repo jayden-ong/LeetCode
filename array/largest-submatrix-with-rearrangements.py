a6ec324065eb24fb81c883c2d56444cdab523d20
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-17 14:32:38 -0400

    LC-largest-submatrix-with-rearrangements [Runtime: N/A; Memory: N/A]

---
+++ array/largest-submatrix-with-rearrangements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        mod_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                curr_val = 0
+                if matrix[i][j] == 1:
+                    if i > 0:
+                        curr_val += mod_matrix[i - 1][j]
+                    curr_val += matrix[i][j]
+                mod_matrix[i][j] = curr_val
+            mod_matrix[i].sort(reverse=True)
+        
+        answer = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                answer = max(answer, mod_matrix[i][j] * (j + 1))
+        return answer
