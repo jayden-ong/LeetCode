f2d8326a8a769d614467404194a92b4ecee9df31
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-24 18:14:21 -0400

    LC-construct-product-matrix [Runtime: N/A; Memory: N/A]

--- prefix-sum/construct-product-matrix.py
+++ prefix-sum/construct-product-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        MOD = 12345
         final = 1
         answer = []
         for i in range(len(grid)):
@@ -11,5 +12,5 @@ class Solution:
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                answer[i][j] = (final // grid[i][j]) % 12345
+                answer[i][j] = (final // grid[i][j]) % MOD
         return answer
