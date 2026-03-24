602d864459002e6c5690357987e30afedf137634
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-24 18:12:04 -0400

    LC-construct-product-matrix [Runtime: N/A; Memory: N/A]

--- array/construct-product-matrix.py
+++ array/construct-product-matrix.py
@@ -1,11 +1,14 @@
 class Solution:
     def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         final = 1
+        answer = []
         for i in range(len(grid)):
+            new_row = []
             for j in range(len(grid[0])):
                 final *= grid[i][j]
-
-        answer = [([1] * len(grid[0])) for _ in range(len(grid))]
+                new_row.append(1)
+            answer.append(new_row)
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 answer[i][j] = (final // grid[i][j]) % 12345
