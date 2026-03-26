bb61bb9fd600c6db7f2d20a625d64610670f61d1
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-26 10:31:03 -0400

    LC-equal-sum-grid-partition-ii [Runtime: 1201 ms; Memory: 59.1 MB]

--- hash-table/equal-sum-grid-partition-ii.py
+++ hash-table/equal-sum-grid-partition-ii.py
@@ -29,13 +29,13 @@ class Solution:
             for j in range(len(grid[0])):
                 grid_set_up[grid[i][j]] += 1
                 grid_set_down[grid[i][j]] -= 1
-            print(desired_element)
+            
             if desired_element < 0:
                 # Only option is the corners if we are looking at the top row
                 if (i == 0 and abs(desired_element) == grid[i][0] or abs(desired_element) == grid[i][len(grid[0]) - 1]) or (0 < i <= len(grid) - 2 and grid_set_up[abs(desired_element)] > 0 and (len(grid[0]) > 1 or grid[0][0] == abs(desired_element))):
                     return True
             else:
-                if (i == len(grid) - 2 and abs(desired_element) == grid[i + 1][0] or abs(desired_element) == grid[i + 1][len(grid[0]) - 1]) or (0 < i <= len(grid) - 2 and grid_set_down[abs(desired_element)] > 0 and (len(grid[0]) > 1 or grid[len(grid) - 1][0] == abs(desired_element))):
+                if (i == len(grid) - 2 and abs(desired_element) == grid[i + 1][0] or abs(desired_element) == grid[i + 1][len(grid[0]) - 1]) or (0 <= i < len(grid) - 2 and grid_set_down[abs(desired_element)] > 0 and (len(grid[0]) > 1 or grid[len(grid) - 1][0] == abs(desired_element))):
                     return True
 
         grid_set_left = defaultdict(int)
@@ -56,7 +56,7 @@ class Solution:
                 if (j == 0 and abs(desired_element) == grid[0][j] or abs(desired_element) == grid[len(grid) - 1][j]) or (0 < j <= len(grid[0]) - 2 and grid_set_left[abs(desired_element)] > 0 and (len(grid) > 1 or grid[0][0] == abs(desired_element))):
                     return True
             else:
-                if (j == len(grid[0]) - 2 and abs(desired_element) == grid[0][j + 1] or abs(desired_element) == grid[len(grid) - 1][j + 1]) or (0 < j <= len(grid[0]) - 2 and grid_set_right[abs(desired_element)] > 0 and (len(grid) > 1 or grid[0][len(grid[0]) - 1] == abs(desired_element))):
+                if (j == len(grid[0]) - 2 and abs(desired_element) == grid[0][j + 1] or abs(desired_element) == grid[len(grid) - 1][j + 1]) or (0 <= j < len(grid[0]) - 2 and grid_set_right[abs(desired_element)] > 0 and (len(grid) > 1 or grid[0][len(grid[0]) - 1] == abs(desired_element))):
                     return True
         return False
             
