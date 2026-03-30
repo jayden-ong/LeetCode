3dcebdd1d1b64417ab8991f895a43f069f384082
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-30 12:53:02 -0400

    LC-check-if-strings-can-be-made-equal-with-operations-ii [Runtime: N/A; Memory: N/A]

---
+++ sorting/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        odd_chars = defaultdict(int)
+        even_chars = defaultdict(int)
+        for i in range(len(s1)):
+            if i % 2 == 0:
+                even_chars[s1[i]] += 1
+            else:
+                odd_chars[s1[i]] += 1
+        
+        for i in range(len(s1)):
+            if s1[i] == s2[i]:
+                if i % 2 == 0:
+                    even_chars[s1[i]] -= 1
+                else:
+                    odd_chars[s1[i]] -= 1
+            else:
+                if i % 2 == 0:
+                    if even_chars[s2[i]] > 0:
+                        even_chars[s2[i]] -= 1
+                    else:
+                        return False
+                else:
+                    if odd_chars[s2[i]] > 0:
+                        odd_chars[s2[i]] -= 1
+                    else:
+                        return False
+        return True
