8786c45e53efe6af16e36ef21471aa0e3d10ede4
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-03-31 14:32:49 -0400

    LC-lexicographically-smallest-generated-string [Runtime: 139 ms; Memory: 19.5 MB]

--- string-matching/lexicographically-smallest-generated-string.py
+++ string-matching/lexicographically-smallest-generated-string.py
@@ -1,33 +1,27 @@
 class Solution:
     def generateString(self, str1: str, str2: str) -> str:
-        answer = []
-        answer_fixed = []
+        answer = ["a"] * (len(str1) + len(str2) - 1)
+        answer_fixed = [False] * (len(str1) + len(str2) - 1)
         # Start by filling in the necessary characters according to "T"
         for i, condition in enumerate(str1):
             if condition == "T":
                 for j in range(i, i + len(str2)):
                     next_char = str2[j - i]
-                    if j >= len(answer):
-                        answer.append(next_char)
-                        answer_fixed.append("T")
-                    else:
-                        if answer[j] != next_char and answer_fixed[j] == "T":
-                            return ""
-                        answer[j] = next_char
-                        answer_fixed[j] = "T"
-            else:
-                if i + len(str2) > len(answer):
-                    num_chars_needed = i + len(str2) - len(answer)
-                    for k in range(num_chars_needed):
-                        answer.append("a")
-                        answer_fixed.append("F")
+                    if answer[j] != next_char and answer_fixed[j] == True:
+                        return ""
+                    answer[j] = next_char
+                    answer_fixed[j] = True
         
         for i, condition in enumerate(str1):
             # Have to make a modification
             if condition == "F" and ''.join(answer[i:i + len(str2)]) == str2:
                 fixed = False
-                for j in range(i, i + len(str2)):
-                    if answer_fixed[j] == "F":
+                #print(i)
+                #print(len(str2))
+                #print(len(answer))
+                #print(''.join(answer[i:i + len(str2)]))
+                for j in range(i + len(str2) - 1, i - 1, -1):
+                    if answer_fixed[j] == False:
                         # Might have to fix
                         answer[j] = "b"
                         fixed = True
