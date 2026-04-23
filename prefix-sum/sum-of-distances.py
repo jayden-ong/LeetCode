ff22667558ca3da4a39472c51482ca6c2b1d9260
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-04-23 08:44:53 -0400

    LC-sum-of-distances [Runtime: 159 ms; Memory: 54.7 MB]

--- prefix-sum/sum-of-distances.py
+++ prefix-sum/sum-of-distances.py
@@ -6,10 +6,9 @@ class Solution:
         
         answer = [0] * len(nums)
         for num in num_to_index:
-            ground = num_to_index[num][0] * len(num_to_index[num])
             for i, index in enumerate(num_to_index[num]):
                 if i == 0:
-                    answer[index] = sum(num_to_index[num]) - ground
+                    answer[index] = sum(num_to_index[num]) - num_to_index[num][0] * len(num_to_index[num])
                 else:
-                    answer[index] = answer[num_to_index[num][i - 1]] - (len(num_to_index[num]) - i - 1) * (num_to_index[num][i] - num_to_index[num][i - 1]) + (i - 1) * (num_to_index[num][i] - num_to_index[num][i - 1]) - ground
+                    answer[index] = answer[num_to_index[num][i - 1]] - (len(num_to_index[num]) - i - 1) * (num_to_index[num][i] - num_to_index[num][i - 1]) + (i - 1) * (num_to_index[num][i] - num_to_index[num][i - 1])
         return answer
