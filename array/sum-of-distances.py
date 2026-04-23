82e8e98783be6d8f3731fc5d3e5175820416f0ff
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-04-23 08:41:32 -0400

    LC-sum-of-distances [Runtime: N/A; Memory: N/A]

--- array/sum-of-distances.py
+++ array/sum-of-distances.py
@@ -1,10 +1,15 @@
 class Solution:
     def distance(self, nums: List[int]) -> List[int]:
         num_to_index = defaultdict(list)
-        answer = [0] * len(nums)
         for i, num in enumerate(nums):
-            for prev_index in num_to_index[num]:
-                answer[prev_index] += abs(prev_index - i)
-                answer[i] += abs(prev_index - i)
             num_to_index[num].append(i)
+        
+        answer = [0] * len(nums)
+        for num in num_to_index:
+            ground = num_to_index[num][0] * len(num_to_index[num])
+            for i, index in enumerate(num_to_index[num]):
+                if i == 0:
+                    answer[index] = sum(num_to_index[num]) - ground
+                else:
+                    answer[index] = answer[num_to_index[num][i - 1]] - (len(num_to_index[num]) - i - 1) * (num_to_index[num][i] - num_to_index[num][i - 1]) + (i - 1) * (num_to_index[num][i] - num_to_index[num][i - 1]) - ground
         return answer
