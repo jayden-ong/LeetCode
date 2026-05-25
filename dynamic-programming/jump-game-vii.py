28975f0941f936d4affce337c442cd42e127c5a2
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-05-25 10:40:42 -0400

    LC-jump-game-vii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game-vii.py
+++ dynamic-programming/jump-game-vii.py
@@ -13,5 +13,6 @@ class Solution:
             
             for j in range(max(i + minJump, end + 1), min(i + maxJump + 1, len(s))):
                 queue.append(j)
+            right = i + minJump
         return False
         
