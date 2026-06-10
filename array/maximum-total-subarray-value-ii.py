c59a90e3a4d641901db5205d1263f2faf7fb0d73
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-06-10 10:33:25 -0400

    LC-maximum-total-subarray-value-ii [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-total-subarray-value-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        max_diffs_heap = []
+        diff_combinations = defaultdict(int)
+        for i in range(len(nums)):
+            curr_min = curr_max = nums[i]
+            for j in range(i + 1, len(nums)):
+                curr_min = min(curr_min, nums[j])
+                curr_max = max(curr_max, nums[j])
+                if -(curr_max - curr_min) not in diff_combinations:
+                    heapq.heappush(max_diffs_heap, -(curr_max - curr_min))
+                diff_combinations[-(curr_max - curr_min)] += 1
+
+        num_selections = k
+        answer = 0
+        while num_selections > 0:
+            curr_diff = heapq.heappop(max_diffs_heap)
+            answer += min(num_selections, diff_combinations[curr_diff]) * -curr_diff
+            num_selections -= min(num_selections, k)
+        return answer
