f98dc72023142664ff20cfe9bf55bb5bcd1386b5
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-06-11 11:38:57 -0400

    LC-number-of-ways-to-assign-edge-weights-i [Runtime: N/A; Memory: N/A]

---
+++ tree/number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        MOD = pow(10, 9) + 7
+        edges_dict = defaultdict(list)
+        for start, end in edges:
+            edges_dict[start].append(end)
+            edges_dict[end].append(start)
+        
+        node_queue = deque()
+        node_queue.append((1, 0))
+        visited = set()
+        max_depth = 0
+        while node_queue:
+            curr_node, curr_distance = node_queue.popleft()
+            max_depth = max(max_depth, curr_distance)
+            visited.add(curr_node)
+            for destination in edges_dict[curr_node]:
+                if destination not in visited:
+                    node_queue.append((destination, curr_distance + 1))
+        
+        answer = 0
+        max_depth_fact = math.factorial(max_depth)
+        for i in range(1, max_depth + 1, 2):
+            answer += max_depth_fact // (math.factorial(i) * math.factorial(max_depth - i))
+            answer = answer % MOD
+        return answer
