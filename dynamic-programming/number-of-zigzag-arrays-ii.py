64b45c2effcc2e6126099ad86ced747664f706a0
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-06-24 10:22:46 -0400

    LC-number-of-zigzag-arrays-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/number-of-zigzag-arrays-ii.py
+++ dynamic-programming/number-of-zigzag-arrays-ii.py
@@ -1,17 +1,42 @@
 class Solution:
     def zigZagArrays(self, n: int, l: int, r: int) -> int:
         MOD = pow(10, 9) + 7
-        num_vals = r - l + 1
-        # How many numbers are less than "i".
-        # Answer : i - l
-        # How many numbers are greater than "i"
-        # Answer : r - i
-        dp_down = [1] * num_vals
-        dp_up = [1] * num_vals
-        for _ in range(n - 1):
-            sum0, sum1 = list(accumulate(dp_down, initial=0)), list(accumulate(dp_up, initial=0))
-            dp_down = [x % MOD for x in sum1[:-1]]
 
-            sum_down = sum0[-1]
-            dp_up = [(sum_down - x) % MOD for x in sum0[1:]]
-        return (sum(dp_down) + sum(dp_up)) % MOD
+        def multiply(matrix1, matrix2):
+            n, m = len(matrix1), len(matrix2[0])
+            answer = [[0] * m for _ in range(n)]
+
+            for i in range(n):
+                for k in range(len(matrix1[0])):
+                    right = matrix1[i][k]
+                    if right == 0:
+                        continue
+                    for j in range(m):
+                        answer[i][j] = (answer[i][j] + right * matrix2[k][j]) % MOD
+            return answer
+        
+        def fast_exp(base, exponent, answer):
+            while exponent > 0:
+                if exponent & 1:
+                    answer = multiply(answer, base)
+                base = multiply(base, base)
+                exponent >>= 1
+            return answer
+
+        num_vals = r - l + 1
+        if n == 1:
+            return num_vals
+        
+        matrix = [[0] * (num_vals * 2) for _ in range(num_vals * 2)]
+        for i in range(num_vals):
+            for j in range(i):
+                matrix[i][j + num_vals] = 1
+            for j in range(i + 1, num_vals):
+                matrix[i + num_vals][j] = 1
+
+        dp = [[1] * (num_vals * 2)]
+        dp = fast_exp(matrix, n - 1, dp)
+        answer = 0
+        for i in range(num_vals * 2):
+            answer = (answer + dp[0][i]) % MOD
+        return answer
