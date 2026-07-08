32e2eabb634df8b379f5402c4cec2df34654781a
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-07-08 12:37:08 -0400

    LC-concatenate-non-zero-digits-and-multiply-by-sum-ii [Runtime: N/A; Memory: N/A]

--- prefix-sum/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ prefix-sum/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+        '''
         MOD = pow(10, 9) + 7
         curr_digits_int, prefix_digits_int = 0, []
         prefix_sum = []
@@ -28,3 +29,31 @@ class Solution:
                 digits_multiple = (prefix_digits_int[right] - prefix_digits_int[left - 1] * ten_pows[diff_in_lengths])
                 answer.append((digits_multiple * curr_sum) % MOD)
         return answer
+        '''
+        MOD, MAX = 1000000007, 100001
+pow = [1] * MAX
+for i in range(1, MAX):
+    pow[i] = (pow[i - 1] * 10) % MOD
+
+class Solution:
+    def sumAndMultiply(self, s: str, queries: list[list[int]]) -> list[int]:
+        n, res = len(s), []
+        A, B, Len = [[0] * (n + 1) for _ in range(3)]
+
+        for i in range(n):
+            d = int(s[i])
+            A[i + 1] = A[i] + d
+            B[i + 1] = (B[i] * 10 + d) % MOD if d else B[i]
+            Len[i + 1] = Len[i] + (d > 0)
+
+        res = []
+
+        for l, r in queries:
+            r += 1
+
+            sub = (B[l] * pow[Len[r] - Len[l]]) % MOD
+            x = (B[r] - sub) % MOD
+
+            res.append((x * (A[r] - A[l])) % MOD)
+
+        return res
