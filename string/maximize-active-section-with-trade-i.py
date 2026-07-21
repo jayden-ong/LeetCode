99fd7d4d55ae86e29bf2767059382aaf401f4841
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-07-21 14:23:48 -0400

    LC-maximize-active-section-with-trade-i [Runtime: N/A; Memory: N/A]

--- string/maximize-active-section-with-trade-i.py
+++ string/maximize-active-section-with-trade-i.py
@@ -2,12 +2,15 @@ class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
         curr, curr_count = s[0], 1
         starting = curr
-        answer = 0
+        active = 0
+        if curr == '1':
+            active += 1
         blocks = [1]
         for i in range(1, len(s)):
             char = s[i]
-            if curr == '1':
-                answer = max(answer, curr_count)
+            if char == '1':
+                active += 1
+            
             if char == curr:
                 curr_count += 1
             else:
@@ -19,23 +22,12 @@ class Solution:
         blocks.append(1)
 
         if len(blocks) <= 4:
-            if starting == '0':
-                if len(blocks) == 3:
-                    return 0
-                return blocks[2]
-            return blocks[1]
+            return active
         
         actual_start = 2
         if starting == '1':
             actual_start = 3
-        
+        answer = 0
         for i in range(actual_start, len(blocks) - 2, 2):
-            curr_answer = 0
-            if i - 2 > 0:
-                curr_answer += blocks[i - 2]
-            
-            if i + 2 < len(blocks) - 1:
-                curr_answer += blocks[i - 2]
-            curr_answer += blocks[i - 1] + blocks[i] + blocks[i + 1]
-            answer = max(answer, curr_answer)
+            answer = max(answer, active + blocks[i - 1] + blocks[i + 1])
         return answer
