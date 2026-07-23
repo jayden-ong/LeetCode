68cc234930d73963189cbfe9a6a8a2b3320c8700
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-07-23 07:15:49 -0400

    LC-number-of-unique-xor-triplets-i [Runtime: N/A; Memory: N/A]

--- bit-manipulation/number-of-unique-xor-triplets-i.py
+++ bit-manipulation/number-of-unique-xor-triplets-i.py
@@ -3,5 +3,5 @@ class Solution:
         if len(nums) <= 2:
             return len(nums)
         
-        highest_bit = math.floor(math.log(len(nums)))
+        highest_bit = math.floor(math.log(len(nums), 2))
         return pow(2, highest_bit + 1)
