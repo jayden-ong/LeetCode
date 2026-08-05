4c8191e0e43acd719b1aeda39a06f43643959d91
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-08-05 14:24:46 -0400

    LC-remove-methods-from-project [Runtime: N/A; Memory: N/A]

---
+++ graph/remove-methods-from-project.py
@@ -0,0 +1,35 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        edges_dict = defaultdict(list)
+        for start, end in invocations:
+            edges_dict[start].append(end)
+        
+        queue = deque()
+        queue.append(k)
+        visited = set()
+        while queue:
+            curr_node = queue.popleft()
+            visited.add(curr_node)
+            for dest in edges_dict[curr_node]:
+                if dest not in visited:
+                    queue.append(dest)
+        
+        answer = []
+        if len(visited) == n:
+            return answer
+        
+        for i in range(n):
+            invalid = False
+
+            if i not in visited:
+                for dest in edges_dict[i]:
+                    if dest in visited:
+                        invalid = True
+                        break
+            
+                if not invalid:
+                    answer.append(i)
+        
+        if answer == []:
+            return [i for i in range(n)]
+        return answer
