21ca5c79c89997128751039bc7bc0dcbbe863316
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-08-12 14:08:29 -0400

    LC-length-of-longest-subarray-with-at-most-k-frequency [Runtime: N/A; Memory: N/A]

---
+++ hash-table/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        right = 0
+        answer = 0
+        curr_freq = defaultdict(int)
+        violation = False
+        for i in range(len(nums)):
+            while right < len(nums) and not violation:
+                curr_freq[nums[right]] += 1
+                if curr_freq[nums[right]] > k:
+                    violation = True
+                right += 1
+
+            answer = max(answer, right - i - 1)
+            
+            curr_freq[nums[i]] -= 1
+            if curr_freq[nums[i]] == k:
+                violation = False
+        return answer
