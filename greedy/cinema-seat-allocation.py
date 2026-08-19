49cea8ddf5fa01f5cf81bf5281f1724f4bac6819
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-08-19 10:52:06 -0400

    LC-cinema-seat-allocation [Runtime: N/A; Memory: N/A]

--- greedy/cinema-seat-allocation.py
+++ greedy/cinema-seat-allocation.py
@@ -1,32 +1,22 @@
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
-        seats_set = set()
+        seats = defaultdict(set)
+        answer = 2 * n
         for (row, seat) in reservedSeats:
-            seats_set.add((row, seat))
-
-        def check_first_block(row):
-            for i in range(2, 6):
-                if (row, i) in seats_set:
-                    return False
-            return True
+            if seat in (2, 3):
+                seats[row].add(0)
+            elif seat in (4, 5):
+                seats[row].add(0)
+                seats[row].add(1)
+            elif seat in (6, 7):
+                seats[row].add(1)
+                seats[row].add(2)
+            elif seat in (8, 9):
+                seats[row].add(2)
         
-        def check_second_block(row):
-            for i in range(4, 8):
-                if (row, i) in seats_set:
-                    return False
-            return True
-        
-        def check_third_block(row):
-            for i in range(6, 10):
-                if (row, i) in seats_set:
-                    return False
-            return True
-
-        answer = 0
-        for i in range(1, n + 1):
-            first_res, second_res, third_res = check_first_block(i), check_second_block(i), check_third_block(i)
-            if first_res and third_res:
-                answer += 2
-            elif first_res or second_res or third_res:
-                answer += 1
+        for row in seats:
+            if len(seats[row]) == 3:
+                answer -= 2
+            else:
+                answer -= 1
         return answer
