3db077283e90e5b71349e62241d73700a766ce31
jayden-ong <90994882+jayden-ong@users.noreply.github.com>
2026-09-19 13:54:59 -0400

    LC-circle-and-rectangle-overlapping [Runtime: N/A; Memory: N/A]

--- geometry/circle-and-rectangle-overlapping.py
+++ geometry/circle-and-rectangle-overlapping.py
@@ -27,6 +27,17 @@ class Solution:
         if (y1 < (yCenter - radius) and y2 < (yCenter - radius)) or (y1 > (yCenter + radius) and y2 > (yCenter + radius)):
             return False
 
+        def check_overlap(x, y):
+            return x1 <= x <= x2 and y1 <= y <= y2
+        
+        def check_within_circle(x, y):
+            return ((x - xCenter) ** 2 + (y - yCenter) ** 2) ** 0.5 <= radius
+        
+        circle_points = [(xCenter + radius, yCenter), (xCenter - radius, yCenter), (xCenter, yCenter + radius), (xCenter, yCenter - radius)]
+        for x, y in circle_points:
+            if check_overlap(x, y):
+                return True
+        
         rectangle_points = [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]
         for x, y in rectangle_points:
             if check_within_circle(x, y):
